abe91948838cd920066813fc5561855fa553a42f
ldonghee <dio.lee@kakaostyle.com>
2026-02-27 20:54:23 +0900

    refactor: 프로그래머스 > bfs > 게임 맵 최단거리

--- src/main/java/com/dhlee/v1/programmers/dfsbfs/GameMap.java
+++ src/main/java/com/dhlee/v1/programmers/dfsbfs/GameMap.java
@@ -1,5 +1,8 @@
 package com.dhlee.v1.programmers.dfsbfs;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * 게임 맵 최단거리
  * https://school.programmers.co.kr/learn/courses/30/lessons/1844
@@ -11,8 +14,8 @@ public class GameMap {
     public int solution(int[][] maps) {
         boolean[][] visited = new boolean[maps.length][maps[0].length];
 
-        for (int i=0; i< maps.length; i++) {
-            for (int j=0; j<maps[i].length; j++) {
+        for (int i = 0; i < maps.length; i++) {
+            for (int j = 0; j < maps[i].length; j++) {
                 if (maps[i][j] == 0) {
                     visited[i][j] = true;
                 }
@@ -48,5 +51,45 @@ public class GameMap {
         recursive(curX, curY - 1, maps, visited, step + 1);
 
         visited[curY][curX] = false;
+    }
+
+    public int solution2(int[][] maps) {
+        int n = maps.length;
+        int m = maps[0].length;
+
+        // 위, 아래, 왼쪽, 오른쪽
+        int[] dx = {0, 0, -1, 1};
+        int[] dy = {-1, 1, 0, 0};
+
+        boolean[][] visited = new boolean[n][m];
+
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{0, 0, maps[0][0]});
+
+        visited[0][0] = true;
+
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int curX = current[0];
+            int curY = current[1];
+            int dist = current[2];
+
+            if (curX == m - 1 && curY == n - 1) {
+                return dist;
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int nextX = curX + dx[i];
+                int nextY = curY + dy[i];
+
+                if (nextX >= 0 && nextX < m && nextY >= 0 && nextY < n) {
+                    if (maps[nextY][nextX] == 1 && !visited[nextY][nextX]) {
+                        visited[nextY][nextX] = true;
+                        queue.add(new int[]{nextX, nextY, dist + 1});
+                    }
+                }
+            }
+        }
+        return -1;
     }
 }
